8b6fbb9c16ea450aaf08df97d758d9685f04e8b3
davidlat <david.osullivan@czechone.com>
2016-07-12 12:23:12 +0100

    Update _footer.blade.php
    Phone update

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -24,9 +24,9 @@
                                         21 Mullaghadun Lane<br />
                                         Dungannon<br />
                                         Co. Tyrone<br />
-                                        BT71 6LH<br />
-                                        Tel -                            UK: 028 8772 5951 or 07884 033323 / 07751 485835<br />
-                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ROI: +4428 8772 5951 or +447884 033323
+                                        BT71 6LH<br /><br />
+                                        Tel -   UK: 028 8772 5951 or 07884 033323 / 07751 485835<br />
+                                        Tel -  ROI: +4428 8772 5951 or +447884 033323
                                         
                                         </p>
 
